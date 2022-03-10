7c206e9801d8c51913b415957c10da0f423696a7
Than1st <1911500641@student.budiluhur.ac.id>
2022-03-10 19:56:42 +0700

    Initial commit

---
+++ app/src/main/java/com/than/challengeschapter2/Gojek.kt
@@ -0,0 +1,12 @@
+package com.than.challengeschapter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+
+class Gojek : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gojek)
+    }
+}
